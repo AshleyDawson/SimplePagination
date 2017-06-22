7ab3702b7e76eb2395b529ea0b9263ebe6e6981d
Ashley Dawson <ashley@ashleydawson.co.uk>
2017-06-22 16:51:46 +0100

    Used internal version of PHPUnit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,4 @@
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
-
 $loader->add('AshleyDawson\SimplePagination\Tests', __DIR__);
