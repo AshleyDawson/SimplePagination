8b7f5e3fcf6015473a7fa6c8846928421360b757
Ashley Dawson <ashley@ashleydawson.co.uk>
2017-06-22 16:48:40 +0100

    Added BC fix for PHPUnit on PHP7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,10 @@
 <?php
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
+
+if (!class_exists('\PHPUnit\Framework\TestCase') &&
+    class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 $loader->add('AshleyDawson\SimplePagination\Tests', __DIR__);
